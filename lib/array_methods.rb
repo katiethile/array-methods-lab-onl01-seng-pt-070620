6f784366af1c73acf8345d86c6f9f1b4220cb307
Katie Le <lethikatie@gmail.com>
2020-07-09 21:08:15 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,26 +4,26 @@ array.include?("wow")
 end
 
 def using_sort(array)
-array = ["Pink", "Blue", "", "", ""]
+array = ["Pink", "Blue", "Green", "Purple", "Gray"]
 array.sort
 end
 
 def using_reverse(array)
-array = ["wow", "I", "am", "really", "learning", "arrays!"]
+array =["Pink", "Blue", "Green", "Purple", "Gray"]
 array.reverse
 end
 
 def using_first(array)
-array = ["wow", "I", "am", "really", "learning", "arrays!"]
+array = ["Pink", "Blue", "Green", "Purple", "Gray"]
 array.first
 end
 
 def using_last(array)
-array = ["wow", "I", "am", "really", "learning", "arrays!"]
+array = ["Pink", "Blue", "Green", "Purple", "Gray"]
 array.last
 end
 
 def using_size(array)
-array = ["wow", "I", "am", "really", "learning", "arrays!"]
+array = ["Pink", "Blue", "Green", "Purple", "Gray"]
 array.size
 end
