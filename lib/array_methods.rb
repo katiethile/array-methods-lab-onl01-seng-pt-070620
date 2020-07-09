97a9228e84f131962a63a5483f9a194dfcec40cc
Katie Le <lethikatie@gmail.com>
2020-07-09 20:47:36 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,15 +9,18 @@ array.sort
 end
 
 def using_reverse(array)
-
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
+array.reverse
 end
 
 def using_first(array)
-
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
+array.first
 end
 
 def using_last(array)
-
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
+array.last
 end
 
 def using_size(array)
